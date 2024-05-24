2dc62364fe246979f20393655c2c511bf1b5eb18
stevenluo05 <luosteven.mail@gmail.com>
2024-05-24 17:50:03 -0400

    homepage formatting and signup and login fix

--- Webpages/homepage.php
+++ Webpages/homepage.php
@@ -3,15 +3,142 @@
 ?>
 
 <!--Add the html code for the home page below (you are in the body tag of the homepage)-->
-<div class="generalspace">
-    <div></div>
-    <div class="grid">
-        <br>
-        <div>This is the homepage</div>
-        <a href="signup.php">Signup</a>
+
+<div class="main1">
+    
+    <a href=signup.php style="color: white;">Just placeholder. Click this to go to signup page.</a>
+
+    <div class="slogan">
+      Let us do the writing.
     </div>
-    <div></div>
-</div>
+
+    <br>
+    <br>
+    <br>
+
+    <div class="grid3">
+
+      <div class="blurb1">
+        Calliope drafts personalized cold emails so you can focus on school, work, and life.
+      </div>
+
+      <div>
+        <img src="../Images/paperairplanecoloured.png" height="300">
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="main2">
+    
+    <div class="grid2">
+
+      <div class="forstudents">
+
+        <div class="subtitle">
+          For students, by students.
+        </div>
+
+        <div class="blurb2">
+          Calliope was created to help you introduce yourself to employers.
+          <br>
+          <br>
+          Cold emailing is part of the game. A long, unnecessarily tedious part.
+          <br>
+          <br>
+          You have to spend hours looking into labs, firms, and people so you can convey genuine interest when you email them.
+          <br>
+          <br>
+          We do all that for you.
+        </div>
+        
+
+      </div>
+
+      <div class="howwork">
+
+        <div class="subtitle">
+          How does Calliope work?
+        </div>
+
+        <div class="blurb2">
+          All you have to do is input links to the websites of the places you're interested in.
+          <br>
+          <br>
+          Calliope will comb those websites for data and use them to write detailed, organization-specific emails.
+          <br>
+          <br>
+          <strong>Then, you send the email and get hired.</strong>
+
+        </div>
+        
+      </div>
+
+    </div>
+    
+
+  </div>
+
+  <div class="main3">
+
+    <div class="pricing">
+      Pricing
+    </div>
+
+    <div class="grid4">
+
+      <div class="blurb3">
+
+        <span class="subtitle2">Academic</span>
+        <ul>
+          <li>$10/month</li>
+          <br>
+          <li>15 research emails/month</li>
+        </ul>
+
+      </div>
+
+      <div class="blurb3">
+
+        <span class="subtitle2">Business</span>
+        <ul>
+          <li>$8/month</li>
+          <br>
+          <li>100 business emails/month</li>
+        </ul> 
+
+      </div>
+
+      <div class="blurb3">
+
+        <span class="subtitle2">Deluxe</span>
+        <ul>
+          <li>$20/month</li>
+          <br>
+          <li>20 research emails/month</li>
+          <br>
+          <li>200 business emails/month</li>
+        </ul> 
+
+      </div>
+
+      <div class="blurb3">
+
+        <span class="subtitle2">Elite</span>
+        <ul>
+          <li>$30/month</li>
+          <br>
+          <li>30 research emails/month</li>
+          <br>
+          <li>500 business emails/month</li>
+        </ul> 
+
+      </div>
+
+    </div>
+
+  </div>
 
 <?php 
     include 'footer-frontend.php';
