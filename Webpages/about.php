bc21c9b97c789db8594c10dfb4f4457daab9163f
stevenluo05 <luosteven.mail@gmail.com>
2024-06-21 22:28:37 -0400

    added about and terms of service page

---
+++ Webpages/about.php
@@ -0,0 +1,36 @@
+<?php 
+    include_once __DIR__.'/head-home.php';
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="width90 auto">
+  <div class="white center worksans weight700 size70">
+    Our mission is to help you get the job you deserve.
+  </div>
+
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php 
+    include 'footer-frontend.php';
+?>
